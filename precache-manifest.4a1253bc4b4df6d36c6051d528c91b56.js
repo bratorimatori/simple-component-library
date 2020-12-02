92ab39159202c3684d30ecbb6c4f661c11b3144a
Bojan Tomic <bojan.tomic@tymeshift.com>
2020-12-02 08:48:38 +0100

    Updates

---
+++ precache-manifest.4a1253bc4b4df6d36c6051d528c91b56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e36d6eab4cae6201980de74d830286",
+    "url": "/bratorimatori/simple-component-library/index.html"
+  },
+  {
+    "revision": "d3543acc15526481d0ac",
+    "url": "/bratorimatori/simple-component-library/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "fbacec3ec0dac4fa953c",
+    "url": "/bratorimatori/simple-component-library/static/js/2.85a1b52b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/bratorimatori/simple-component-library/static/js/2.85a1b52b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3543acc15526481d0ac",
+    "url": "/bratorimatori/simple-component-library/static/js/main.37a0464e.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/bratorimatori/simple-component-library/static/js/main.37a0464e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa65610274244b0073a6",
+    "url": "/bratorimatori/simple-component-library/static/js/runtime-main.88ce5e92.js"
+  }
+]);
